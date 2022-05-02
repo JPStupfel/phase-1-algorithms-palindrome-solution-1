1ae056abdead9335053e744060ef91f7266fa387
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-05-01 20:07:44 -0500

    commit tree dcb89458ab9af03dbd5b27a1728105ac62d63fb1

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+  return word === word.split('').reverse().join('')
 }
 
 /* 
   Add your pseudocode here
+Reverse the input.
+
+check if reversed input is same as original.
+
+if so, return true.
+
+otherwise, return false.
+
+
+
 */
 
 /*
   Add written explanation of your solution here
+
+  Ok. So we need a function that takes a word and checks it against the reverse of itself. Meaning mom should return true and bad should return false. Those are actually the only two steps we need. step one, reverse the word. Step two, return the boolean of that word checked against itself.
+
 */
 
 // You can run `node index.js` to view these console logs
